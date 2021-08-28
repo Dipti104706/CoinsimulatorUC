ae5eed01549a63517130f5c0be4af3736b969065
Dipti104706 <diptimayeebehura104@gmail.com>
2021-08-28 21:23:14 +0530

    Added: code to check it is tie or not till difference of 2

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -3,10 +3,11 @@
 headsCount=0
 tailsCount=0
 count=0
+
 while (( $headsCount<21 && $tailsCount<21 ))
 do	
 	flip=$(($((RANDOM%10))%2))
-  	if [ $flip -eq 1 ]
+ 	if [ $flip -eq 1 ]
 	then
 		echo "Heads"
 		headsCount=$(($headsCount+1))
@@ -16,16 +17,23 @@ do
 		count=$(($count+1))
 	fi
 done
-echo "Heads=$headsCount"
-echo "Tails=$tailsCount"
+	echo "Heads=$headsCount"
+	echo "Tails=$tailsCount"
 if [ $headsCount -gt $tailsCount ]
 then
-	winHead=$(($headsCount-$tailsCount))
-	echo "Heads Won and Win difference is $winHead"
-elif [ $headsCount -lt $tailsCount ]
+	minDiff=$(($headsCount-$tailsCount))
+	echo "Heads Won and Win difference is $minDiff"
+elif
+	[ $headsCount -lt $tailsCount ]
 then
-	winTail=$(($tailsCount-$headsCount))
-	echo "Tails Won and Win difference is $winTail"
-else
-echo "Tie"
+	minDiff=$(($tailsCount-$headsCount))
+	echo "Tails Won and Win difference is $minDiff"
+elif [ $headsCount -eq $tailsCount ]
+then
+	echo"Tie"
+	while (($minDiff>=3))
+	do
+		continue;
+	done
 fi
+
